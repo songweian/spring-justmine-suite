659f4fd56c5b73afabdff2a0750f033d15f927b3
weian404 <weian404@gmail.com>
2023-07-20 23:44:46 +0800

    init project

--- spring-justmine-commons/src/main/java/org/justmine/spring/common/http/HttpHeaders.java
+++ spring-justmine-commons/src/main/java/org/justmine/spring/common/http/HttpHeaders.java
@@ -1,4 +1,45 @@
 package org.justmine.spring.common.http;
 
 public class HttpHeaders {
+
+    public static final String ACCEPT = "Accept";
+    public static final String ACCEPT_CHARSET = "Accept-Charset";
+    public static final String ACCEPT_ENCODING = "Accept-Encoding";
+    public static final String ACCEPT_LANGUAGE = "Accept-Language";
+    public static final String ACCEPT_RANGES = "Accept-Ranges";
+    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+    public static final String AGE = "Age";
+    public static final String ALLOW = "Allow";
+
+    public static final String TRACE_ID = "X-B3-TraceId";
+    public static final String SPAN_ID = "X-B3-SpanId";
+    public static final String PARENT_SPAN_ID = "X-B3-ParentSpanId";
+    public static final String SAMPLED = "X-B3-Sampled";
+    public static final String FLAGS = "X-B3-Flags";
+
+    public static final String X_FORWARDED_FOR = "X-Forwarded-For";
+    public static final String X_FORWARDED_HOST = "X-Forwarded-Host";
+    public static final String X_FORWARDED_PORT = "X-Forwarded-Port";
+    public static final String X_FORWARDED_PROTO = "X-Forwarded-Proto";
+    public static final String X_FORWARDED_SERVER = "X-Forwarded-Server";
+
+    public static final String X_REQUEST_ID = "X-Request-Id";
+    public static final String X_CORRELATION_ID = "X-Correlation-Id";
+
+    public static final String X_FRAME_OPTIONS = "X-Frame-Options";
+    public static final String X_XSS_PROTECTION = "X-XSS-Protection";
+    public static final String X_CONTENT_TYPE_OPTIONS = "X-Content-Type-Options";
+    public static final String X_PERMITTED_CROSS_DOMAIN_POLICIES = "X-Permitted-Cross-Domain-Policies";
+    public static final String REFERRER_POLICY = "Referrer-Policy";
+    public static final String STRICT_TRANSPORT_SECURITY = "Strict-Transport-Security";
+    public static final String CONTENT_SECURITY_POLICY = "Content-Security-Policy";
+    public static final String CONTENT_SECURITY_POLICY_REPORT_ONLY = "Content-Security-Policy-Report-Only";
+    public static final String X_CONTENT_SECURITY_POLICY = "X-Content-Security-Policy";
 }
