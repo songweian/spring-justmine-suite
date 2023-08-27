0611f966a7c2633a0d8603269204bb0665113574
weian404 <weian404@gmail.com>
2023-08-27 17:33:33 +0800

    update

---
+++ justmine-spring-boot-examples/boot-example-stater-web/src/main/java/pro/justmine/springboot/example/starter/web/JustmineExampleWebApplication.java
@@ -0,0 +1,11 @@
+package pro.justmine.springboot.example.starter.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmineExampleWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JustmineExampleWebApplication.class, args);
+    }
+}
