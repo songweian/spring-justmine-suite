49081511815f9ee9fefbd79488e431ddde8c5835
weian404 <weian404@gmail.com>
2023-08-27 17:31:48 +0800

    update

---
+++ justmine-spring-boot-examples/boot-example-stater/src/main/java/pro/justmine/springboot/example/starter/JustmineExampleApplication.java
@@ -0,0 +1,11 @@
+package pro.justmine.springboot.example.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustmineExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JustmineExampleApplication.class, args);
+    }
+}
--- justmine-spring-client/src/main/java/pro/justmine/spring/client/JustmineEnvironmentProcessor.java
+++ justmine-spring-client/src/main/java/pro/justmine/spring/client/JustmineEnvironmentProcessor.java
@@ -3,10 +3,13 @@ package pro.justmine.spring.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
 
 public class JustmineEnvironmentProcessor implements EnvironmentPostProcessor {
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        MutablePropertySources propertySources = environment.getPropertySources();
+//        propertySources.addFirst(new JustmineInfraPropertySource("justmine-infra", System.getenv()));
     }
 }
